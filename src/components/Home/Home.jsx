614860724928e8628e1aeca48039c2cd9dfce49d
UGSNBandara <sulithanb119@gmail.com>
2024-12-13 20:18:54 +0530

    3rd commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+      <div>
+          <H1>Hello this is me</H1>
+      </div>
+  
+    );
+  };
+  
+  export default Home;
+  
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Quiz-Management-system-GUI-/",
   plugins: [react()],
 })
